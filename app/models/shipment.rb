307d5ee2c3a18b346ec715f418d2a44dd0f4a8d2
= <=>
2020-02-25 06:04:24 -0500

    Enable Shipment deletion

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,5 +1,5 @@
 class Shipment < ApplicationRecord
-  has_many :histories
+  has_many :histories, dependent: :destroy
   def self.search(search)
     if search.present?
       # where("trackingnumber @@ :q", q: search)
